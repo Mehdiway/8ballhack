8edf6a3894c57ed6535e208076dd73089b115198
Mehdiway <armachi.mehdi@gmail.com>
2017-09-17 16:39:46 +0100

    Fixed distances and added support for zoomed pages
    Added support for zoomed pages for added precision

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,53 +2,76 @@
 // Instead a sketch instance has to be manually created
 // This is done with the closure below
 
-var sketch = function(p5) {
-  // setup canvas
-  p5.setup = function() {
-    console.log('p5 running');
-    var c = p5.createCanvas(p5.windowWidth, p5.windowHeight);
-    c.style('pointer-events', 'none');
-    c.position(offsetLeft + 90, offsetTop + 140);
-    p5.clear();
-    p5.noFill();
-    p5.noLoop();
-    p5.stroke(255, 30);
-  }
-  // draw the guidelines
-  p5.draw = function() {
-		p5.clear();
-    // set stroke weight
-    p5.strokeWeight(2);
-    // set transparency
-    p5.stroke(255, 30);
-		p5.line(0,0, p5.mouseX, p5.mouseY);
-		p5.line(285,0, p5.mouseX, p5.mouseY);
-		p5.line(570,0, p5.mouseX, p5.mouseY);
-		p5.line(0,274, p5.mouseX, p5.mouseY);
-		p5.line(285,274, p5.mouseX, p5.mouseY);
-		p5.line(570,274, p5.mouseX, p5.mouseY);
-    // increase the transparency of the circle
-    p5.stroke(255, 100);
-		p5.ellipse(p5.mouseX, p5.mouseY, 16);
-	}
-  // change the layout if mouse is moved and SHIFT key is pressed
-  p5.mouseMoved = function() {
-    if(p5.keyIsDown(p5.SHIFT)){
-      p5.draw();
-    }
-  }
-  p5.keyPressed = function() {
-    if(p5.keyIsDown(p5.SHIFT)){
-      p5.draw();
-    }
-  }
+// Support zoom : Percent instead of pixels
+var tableWidthPerc = 0.7626;
+var tableHeightPerc = 0.5403;
+// Offset
+var tableOffsetTopPerc = 0.32; // %
+var tableOffsetLeftPerc = 0.12; // %
 
+var container = document.getElementById('gm-wrap').getBoundingClientRect();
+
+var tableWidth = container.width * tableWidthPerc;
+var tableHeight = container.height * tableHeightPerc;
+
+var off_top = container.top + container.height * tableOffsetTopPerc,
+	off_left = container.left + container.width * tableOffsetLeftPerc,
+	off_right = off_left + tableWidth,
+	off_bottom = off_top + tableHeight,
+	off_middle = off_left + tableWidth / 2;
+
+var mouseOffset = 8;
+
+var activate = true;
+
+var sketch = function(p5) {
+    // setup canvas
+    p5.setup = function() {
+        console.log('p5 running');
+        var c = p5.createCanvas(tableWidth, tableHeight);
+        c.style('pointer-events', 'none');
+        c.position(off_left, off_top);
+		console.log(off_left, off_top);
+        p5.clear();
+        p5.noFill();
+        p5.noLoop();
+        p5.stroke(255, 30);
+    }
+    // draw the guidelines
+    p5.draw = function() {
+        p5.clear();
+
+		if (!activate) return;
+        // set stroke weight
+        p5.strokeWeight(2);
+        // set transparency
+        p5.stroke(255, 30);
+        p5.line(0, 0, p5.mouseX - mouseOffset, p5.mouseY - mouseOffset);
+        p5.line(tableWidth / 2, 0, p5.mouseX - mouseOffset, p5.mouseY - mouseOffset);
+        p5.line(tableWidth, 0, p5.mouseX - mouseOffset, p5.mouseY - mouseOffset);
+        p5.line(0, tableHeight, p5.mouseX - mouseOffset, p5.mouseY - mouseOffset);
+        p5.line(tableWidth / 2, tableHeight, p5.mouseX - mouseOffset, p5.mouseY - mouseOffset);
+        p5.line(tableWidth, tableHeight, p5.mouseX - mouseOffset, p5.mouseY - mouseOffset);
+        // increase the transparency of the circle
+        p5.strokeWeight(1);
+        p5.stroke(255, 255);
+        p5.ellipse(p5.mouseX -  mouseOffset, p5.mouseY -  mouseOffset, 18);
+    }
+    // change the layout if mouse is moved and SHIFT key is pressed
+    p5.mouseMoved = function() {
+        if (p5.keyIsDown(p5.SHIFT)) {
+            p5.draw();
+        }
+    }
+    p5.keyPressed = function() {
+        if (p5.keyIsDown(p5.SHIFT)) {
+            p5.draw();
+        } else if (p5.keyIsDown(80)) { // P
+			activate = !activate;
+	        p5.draw();
+		}
+    }
 }
-// find the game element and its position
-var bodyRect = document.body.getBoundingClientRect(),
-    elemRect = document.getElementById('gm-wrap').getBoundingClientRect(),
-    offsetTop = elemRect.top - bodyRect.top;
-    offsetLeft = elemRect.left - bodyRect.left;
 
 // The above function closure is passed into a p5 object constructor
 // this starts the sketch.
